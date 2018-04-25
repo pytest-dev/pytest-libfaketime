6f04dc6d0028d7867b700c2e54484ee97bcb6d84
Éloi Rivard <eloi@yaal.fr>
2018-04-25 14:31:11 +0200

    Bumped to 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.1.1 - 2018-04-25
+
+- Support for pdb. Thanks to @touilleMan
+
+# 0.1.0 - 2017-09-07
+
+- Initial release
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='pytest-libfaketime',
-    version='0.1.0',
+    version='0.1.1',
     author='Éloi Rivard',
     author_email='eloi@yaal.fr',
     maintainer='Éloi Rivard',
