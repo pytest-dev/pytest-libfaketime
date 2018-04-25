3792ea9c726cc6a17215049caddea1ab259e4c3b
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-04-25 11:29:57 +0200

    Fix pytest std streams capture when reload is needed

--- pytest_libfaketime.py
+++ pytest_libfaketime.py
@@ -3,7 +3,11 @@
 import os
 import libfaketime
 
-def pytest_configure():
-    libfaketime.reexec_if_needed()
-    _, env_additions = libfaketime.get_reload_information()
-    os.environ.update(env_additions)
+
+def pytest_configure(config):
+    needs_reload, env_additions = libfaketime.get_reload_information()
+    if needs_reload:
+        capture_manager = config.pluginmanager.getplugin('capturemanager')
+        capture_manager.stop_global_capturing()
+        libfaketime.reexec_if_needed()
+        os.environ.update(env_additions)
