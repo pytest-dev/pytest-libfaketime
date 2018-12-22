98860add8dcd62104e8e32243ea39f3fbeb2e77f
Éloi Rivard <azmeuk@gmail.com>
2018-12-22 18:59:59 +0100

    Updated the repository url

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# pytest-libfaketime [![Build Status](https://travis-ci.org/azmeuk/pytest-libfaketime.svg?branch=master)](https://travis-ci.org/azmeuk/pytest-libfaketime) [![Latest Version](https://img.shields.io/pypi/v/pytest-libfaketime.svg)](https://pypi.python.org/pypi/pytest-libfaketime)
+# pytest-libfaketime [![Latest Version](https://img.shields.io/pypi/v/pytest-libfaketime.svg)](https://pypi.python.org/pypi/pytest-libfaketime)
 [python-libfaketime](https://github.com/simon-weber/python-libfaketime) plugin for pytest, with support for [pytest-xdist](https://github.com/pytest-dev/pytest-xdist).
 
 # Installation
+
 ```pip install pytest-libfaketime```
 
 # Tests
+
 ```tox```
--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
-import io
 from setuptools import setup
 
 setup(
     name='pytest-libfaketime',
-    version='0.1.1',
+    version='0.1.2',
     author='Éloi Rivard',
     author_email='eloi@yaal.fr',
-    maintainer='Éloi Rivard',
-    maintainer_email='eloi@yaal.fr',
+    maintainer='The Yaal Team',
+    maintainer_email='contact@yaal.fr',
     license='MIT',
-    url='https://gitlab.com/azmeuk/pytest-libfaketime',
+    url='https://gitlab.com/yaal/pytest-libfaketime',
     description='A python-libfaketime plugin for pytest.',
-    long_description=io.open('README.md').read(),
+    long_description=open('README.md').read(),
     py_modules=['pytest_libfaketime'],
     install_requires=[
         'libfaketime',
@@ -32,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Operating System :: OS Independent',
