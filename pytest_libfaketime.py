416a687b67e8d0c847e6c4762a605b7f0b74f063
Éloi Rivard <eloi@yaal.coop>
2024-04-03 17:15:59 +0200

    chore: pre-commit

--- pytest_libfaketime.py
+++ pytest_libfaketime.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-
 import os
+
 import libfaketime
 
 
 def pytest_configure(config):
     needs_reload, env_additions = libfaketime.get_reload_information()
     if needs_reload:
-        capture_manager = config.pluginmanager.getplugin('capturemanager')
+        capture_manager = config.pluginmanager.getplugin("capturemanager")
         capture_manager.stop_global_capturing()
         libfaketime.reexec_if_needed()
         os.environ.update(env_additions)
--- test_foobar.py
+++ test_foobar.py
@@ -1,17 +1,22 @@
 import datetime
+
 from libfaketime import fake_time
+
 
 def test_faketime_1():
     with fake_time("2016-05-05 00:00:00"):
         assert datetime.datetime.now() == datetime.datetime(2016, 5, 5, 0, 0, 0)
 
+
 def test_faketime_2():
     with fake_time("2016-05-05 00:00:00"):
         assert datetime.datetime.now() == datetime.datetime(2016, 5, 5, 0, 0, 0)
 
+
 def test_faketime_3():
     with fake_time("2016-05-05 00:00:00"):
         assert datetime.datetime.now() == datetime.datetime(2016, 5, 5, 0, 0, 0)
+
 
 def test_faketime_4():
     with fake_time("2016-05-05 00:00:00"):
